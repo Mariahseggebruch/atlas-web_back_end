3f1ab55d922a98808936eb01dee63ce85d96bc1b
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-01-05 20:31:55 -0800

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-""" Async Generator """
+'''Script create an async generator'''
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """
-        Generate numbers
-
-        Args:
-            void
-
-        Return:
-            float time random
-    """
+    '''function loops 10 times, each time
+    has an asynchronous wait for 1 second, and yeild
+    a random float between 0 and 10'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
