d3fbb9156c65232d80583922ad7d987efdc822f4
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-01-19 15:45:08 +0000

    task 1

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -12,18 +12,29 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
 auth = None
-auth_type = os.getenv('AUTH_TYPE', 'basic')
-
-if auth_type == 'auth':
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-
-if auth_type == 'basic_auth':
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ error handler for (unauthorized) 401 status code """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ error handler for (forbidden) 403 status code """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
@@ -33,45 +44,23 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """
-    forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-# Create an error handler for 401 Unauthorized
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """
-    unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
-def unauthorized_route():
-    """
-    unauthorized route - abort
-    """
-    abort(401)
-
-
 @app.before_request
-def before_request():
-    """
-    runs before every request
-    """
-    path_list = ['/api/v1/status/',
-                 '/api/v1/unauthorized',
-                 '/api/v1/forbidden']
-    if auth and auth.require_auth(request.path, path_list):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+def before_request() -> str:
+    """ method to handler before request """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
