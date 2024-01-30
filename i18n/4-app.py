e98a321ca0e7e6f93215e1e3fc8021da80a6ff7b
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-01-30 17:40:48 +0000

    cant figure this out

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask App """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _ as get_translation
 
 class Config(object):
     """
@@ -41,7 +41,6 @@ def root():
         Returns a rendered template of an
         HTML site
     """
-    from flask_babel import _ as get_tranlsation #importing inside the function
     return render_template('/4-index.html',
                            title=get_translation('home_title'),
                            heading=get_translation('home_header'))
