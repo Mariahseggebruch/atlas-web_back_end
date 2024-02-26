dfec71551fd418d7dcaab96e38b862a148017738
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-02-26 10:45:37 -0600

    task 8

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,3 @@
-// 8.2 Created a class named AppController that will be used to manage the server
-
 class AppController {
     static getHomepage(req, res) {
       res.status(200).send('Hello Holberton School!');
